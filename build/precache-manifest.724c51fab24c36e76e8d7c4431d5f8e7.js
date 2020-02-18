a56f3e55fcb10fb7d22f5b32321990a8cc5c3f47
Steven Mann <stevencmann2@gmail.com>
2020-02-18 11:27:53 -0600

    changing file tree

---
+++ build/precache-manifest.724c51fab24c36e76e8d7c4431d5f8e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73676d240aa731593a31f2fc2b248398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e3eea518ecc68c950d",
+    "url": "/static/js/2.e967dc3f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e967dc3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5622ce3d7d6e73a2baec",
+    "url": "/static/js/main.87c98f82.chunk.js"
+  },
+  {
+    "revision": "cc2927c6818dc6c4f663",
+    "url": "/static/js/runtime-main.f75059d1.js"
+  },
+  {
+    "revision": "3243de69f07c0bca94880e2c9e720e88",
+    "url": "/static/media/bassFish.3243de69.jpg"
+  },
+  {
+    "revision": "615a91ed93276e920568d7dad758d92a",
+    "url": "/static/media/scales.615a91ed.jpg"
+  }
+]);
